6801bf4101bc8d44b172ceae69b86f14efe95b5c
Diego Rosales <diego.rosales@iovlabs.org>
2022-10-27 23:42:43 -0600

    feat: removed build files

--- services/build/models/meme-model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Meme = void 0;
-const mongoose_1 = require("mongoose");
-const MemeSchema = new mongoose_1.Schema({
-    data: { type: String, required: true },
-});
-exports.Meme = (0, mongoose_1.model)("Meme", MemeSchema);
